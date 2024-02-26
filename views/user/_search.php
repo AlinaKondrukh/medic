e8021c09e50305ea41c0845891cd19e31c759d81
Alina <alia.kondruh@gmail.com>
2024-02-26 16:51:54 +0300

    Создаем UserController (UserSearch, /user/_form, _search), копируем Create и меняем его. UserController удаляем

---
+++ views/user/_search.php
@@ -0,0 +1,40 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/** @var yii\web\View $this */
+/** @var app\models\UserSearch $model */
+/** @var yii\widgets\ActiveForm $form */
+?>
+
+<div class="user-search">
+
+    <?php $form = ActiveForm::begin([
+        'action' => ['index'],
+        'method' => 'get',
+        'options' => [
+            'data-pjax' => 1
+        ],
+    ]); ?>
+
+    <?= $form->field($model, 'id') ?>
+
+    <?= $form->field($model, 'fio') ?>
+
+    <?= $form->field($model, 'password') ?>
+
+    <?= $form->field($model, 'date_of_birth') ?>
+
+    <?= $form->field($model, 'tel') ?>
+
+    <?php // echo $form->field($model, 'role_id') ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Search', ['class' => 'btn btn-primary']) ?>
+        <?= Html::resetButton('Reset', ['class' => 'btn btn-outline-secondary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
